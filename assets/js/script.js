99183ced6ed674d05182aa1fcc2fcff0d2c79db0
unknown <mvventurini9@gmail.com>
2022-07-26 17:23:27 -0300

    Atualização javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,10 @@ por Seletor: querySelector() -> melhor prática para acessar o DOM no JavaScript
 let nome = window.document.getElementById("nome")
 let email =  document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -25,6 +29,7 @@ function validaNome() {
     } else{
         txtNome.innerHTML = "Nome Válido"
         txtNome.style.color = "green"
+        nomeOk = true
     }
 }
 function validaEmail(){
@@ -36,6 +41,39 @@ function validaEmail(){
     }else{
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = "green"
+        emailOk = true
     }
+}
+function validaAssunto() {
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if(assunto.value.length >= 100){
+        txtAssunto.innerHTML = " Texto muito grande, digite no máximo 100 caracteres "
+        txtAssunto.style.color = "red"
+        txtAssunto.style.display = "block"
+    }else {
+        txtAssunto.style.display = "none"
+        assuntoOk = true
+    }
+
+}
+
+function enviar () {
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert("Formulário enviado com sucesso!")
+    }else {
+        alert("Preencha o formulário corretamente antes de enviar...")
+    }
+        
+}
+
+function mapazoom(){
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+
+function mapanormal(){
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
 }
 
